ad99893b0985f548f37eec3bc9a50d8de746e79b
Carlos Eduardo <dudu_carlos.eduardo@hotmail.com>
2022-02-20 19:06:11 -0300

    Criado Componente do botão do Icone

---
+++ layoutdp/lib/components/my_custom_icon_button.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+import '../../constant.dart';
+
+class MyCustomIconButton extends StatelessWidget {
+  final double width;
+  final double height;
+  final Icon icon;
+  final Color background;
+  final Color borderColor;
+  final double radius;
+  const MyCustomIconButton({
+    Key? key,
+    required this.width,
+    required this.height,
+    required this.icon,
+    required this.background,
+    required this.borderColor,
+    required this.radius,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      height: height,
+      decoration: BoxDecoration(
+        color: background,
+        border: Border.all(color: borderColor),
+        borderRadius: BorderRadius.circular(radius),
+      ),
+      child: (
+        IconButton(
+          onPressed: () {}, 
+          icon: icon
+        )
+      ),
+    );
+  }
+}
