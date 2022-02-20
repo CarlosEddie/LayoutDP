a624bf5c66a875fb3170e194cfcb7bfd4f5b0693
Carlos Eduardo <dudu_carlos.eduardo@hotmail.com>
2022-02-20 19:06:43 -0300

    Criado Componete de Novo Filme

---
+++ layoutdp/lib/components/my_new_movie.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+
+import '../../constant.dart';
+import 'my_custom_icon_button.dart';
+
+class MyNewMovie extends StatelessWidget {
+  final String image;
+  final String title;
+  final String time;
+  final String rating;
+  const MyNewMovie({
+    Key? key,
+    required this.image,
+    required this.title,
+    required this.time,
+    required this.rating,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(
+            width: 189,
+            height: 202,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              color: Colors.white,
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Container(
+                  width: 165.0,
+                  height: 117.0,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                    image: DecorationImage(
+                      image: AssetImage(image),
+                      fit: BoxFit.fill
+                    ),
+                  ),
+                ),
+                Text(
+                  title,
+                  style: TextStyle(
+                    fontSize: 12.0,
+                    color: Colors.black,
+                  ),
+                ),
+                Container(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text(
+                        time,
+                        style: TextStyle(
+                          fontSize: 12.0,
+                          color: Colors.black,
+                        ),
+                      ),
+                      MyCustomIconButton(
+                        width: 40, 
+                        height: 38, 
+                        icon: Icon (
+                          Icons.play_arrow,
+                          color: Colors.white,
+                        ), 
+                        background: deepBlue, 
+                        borderColor: deepBlue, 
+                        radius: 12,
+                      ),
+                      Container(
+                        child: Row(
+                          children: [
+                            Text(
+                              rating,
+                              style: TextStyle(
+                                fontSize: 12.0,
+                                color: Colors.black,
+                              ),
+                            ),
+                            Icon(
+                              Icons.star,
+                              color: Color(0xFFFFB800),
+                              size: 12,
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
