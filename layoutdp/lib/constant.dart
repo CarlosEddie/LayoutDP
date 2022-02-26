985028f09571f24492d6018498c54ada74e48e32
Carlos Eduardo <dudu_carlos.eduardo@hotmail.com>
2022-02-26 14:56:34 -0300

    Adicionada nova cor

--- layoutdp/lib/constant.dart
+++ layoutdp/lib/constant.dart
@@ -6,4 +6,4 @@ const Color deepBlue = Color(0xFF00147A);
 const Color loginGrey = Color(0xFF707375);
 const Color starYellow = Color(0xFFFFB800);
 const Color lightBlue = Color(0xFF11FFFF);
-
+const Color notificationRed = Color(0xFFFF4141);
